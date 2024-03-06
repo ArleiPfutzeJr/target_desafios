7509bf81b707fc3c8bef7e67e1c63b2793a2c606
ArleiPfutzeJr <arleipfutze.ap@gmail.com>
2024-03-06 19:38:33 -0300

    Exercicios_4.java Finalizado

---
+++ src/target/desafios/Exercicios_4.java
@@ -0,0 +1,16 @@
+package target.desafios;
+//4) Você está em uma sala com três interruptores, cada um conectado a uma lâmpada em uma sala diferente.
+// Você não pode ver as lâmpadas da sala em que está, mas pode ligar e desligar os interruptores quantas vezes quiser.
+// Seu objetivo é descobrir qual interruptor controla qual lâmpada.
+//        Como você faria para descobrir, usando apenas duas idas até uma das salas das lâmpadas, qual interruptor controla cada lâmpada?
+public class Exercicios_4 {
+    public static void main(String[] args) {
+        System.out.println("Bem, se eu fosse encarar esse problema do jeito que eu lidava antigamente, " +
+                "provavelmente eu iria fazer o seguinte:Conforme a ordem das teclas, eu nomearia A, B e C, e as lâmpadas seriam A, B e C. " +
+                "Eu faria assim: ligaria os interruptores A e B e deixaria por um tempo." +
+                " Depois, desligaria o B, pegaria uma escada e sairia da sala." +
+                " Agora eu sei que o interruptor C é o que a lâmpada está desligada e de temperatura fria, pois nunca foi ligado." +
+                " O interruptor B é da lâmpada que está desligada, mas quente, já que até pouco tempo estava ligada." +
+                " E a lâmpada A é a que está acesa. Ainda bem que sou Eletrotecnico formado kkkk Boa Sorte para todos");
+    }
+}
