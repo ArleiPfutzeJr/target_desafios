ea87c2e4d53654d49acf2b96d283256667b0cd9d
ArleiPfutzeJr <arleipfutze.ap@gmail.com>
2024-03-06 19:00:21 -0300

    Exercicios_1.java Finalizado

--- src/target/desafios/Exercicios_1.java
+++ src/target/desafios/Exercicios_1.java
@@ -1,2 +1,24 @@
-package target.desafios;public class Exercicios_1 {
+//1) Observe o trecho de código abaixo:
+//        int INDICE = 13, SOMA = 0, K = 0;
+//        enquanto K < INDICE faça
+//        {
+//        K = K + 1;
+//        SOMA = SOMA + K;
+//        }
+//        imprimir(SOMA);
+//        Ao final do processamento, qual será o valor da variável SOMA?
+package target.desafios;
+public class Exercicios_1 {
+    public static void main(String[] args) {int INDICE=13;
+     int SOMA=0;
+     int k=0;
+
+     while ( k < INDICE){
+        k++;
+        SOMA+=k;
+     }
+        System.out.println("O valor da variável soma é " + SOMA);
+
+    }
+
 }
